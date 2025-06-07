5014083ebe6492191df29d56f1d00a5f815530cb
Bruno <bruno@dell>
2025-06-07 11:02:35 +0100

    Update flux-bair.sh

--- flux-bair.sh
+++ flux-bair.sh
@@ -178,8 +178,11 @@ function provisioning_download() {
         auth_token="$CIVITAI_TOKEN"
     fi
     if [[ -n $auth_token ]];then
-        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1&token=$auth_token"
+	echo wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1&token=$auth_token"
+        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1&token=$auth_token"
     else
+	echo "Oh fuck, there is no token for some reason."
+	echo wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
         wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
     fi
 }
